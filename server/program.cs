cee2b185c4e86857c249ddfa196760aace8fef17
s0m1k111 <spavel1207@gmail.com>
2026-01-19 14:45:21 +0300

    Создание базы програм кс

---
+++ server/program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Приветствую вас.");
+Console.WriteLine("Селиванов П.А. Беляев В.А.");
+Console.WriteLine("ИСП-233");
+Console.WriteLine(DateTime.Now);
+
