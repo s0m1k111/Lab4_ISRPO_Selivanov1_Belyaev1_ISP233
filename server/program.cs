bccafdfc6de8444c7c75e967025fa6fbbd6272f2
s0m1k111 <spavel1207@gmail.com>
2026-01-19 14:52:45 +0300

    Добавление меню

--- server/program.cs
+++ server/program.cs
@@ -2,4 +2,14 @@
 string? fio = "Селиванов П.А. Беляев В.А.";
 string? group = "ИСП-233";
 string? time = DateTime.Now;
-
+int peak = Console.Write(Console.ReadLine());
+switch (peak)
+{
+    case 1:
+        Console.WriteLine(fio); break;
+    case 2:
+        Console.WriteLine(group); break;
+    case 3:
+        Console.WriteLine(time); break;
+    case 4:; break;
+}
