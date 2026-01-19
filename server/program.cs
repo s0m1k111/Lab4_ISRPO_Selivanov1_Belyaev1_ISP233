889c40f82ee5e14d3edecf181dc0289677999ea4
s0m1k111 <spavel1207@gmail.com>
2026-01-19 15:03:19 +0300

    Добавление логики выбора

--- server/program.cs
+++ server/program.cs
@@ -1,8 +1,10 @@
 ﻿Console.WriteLine("Приветствую вас.");
 string? fio = "Селиванов П.А. Беляев В.А.";
 string? group = "ИСП-233";
-string? time = DateTime.Now;
-int peak = Console.Write(Console.ReadLine());
+string time = DateTime.Now.ToString();
+Console.Write("Введите число от 1 до 4: ");
+string? input = Console.ReadLine();
+int peak = Convert.ToInt32(input);
 switch (peak)
 {
     case 1:
@@ -11,5 +13,5 @@ switch (peak)
         Console.WriteLine(group); break;
     case 3:
         Console.WriteLine(time); break;
-    case 4: break;
+    case 4: Console.WriteLine("Exit"); break;
 }
