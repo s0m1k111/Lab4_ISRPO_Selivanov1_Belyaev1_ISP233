736a88ccd67167019eea3694d99e2e29b6508a83
s0m1k111 <spavel1207@gmail.com>
2026-01-19 14:49:35 +0300

    Приветствие и ФИО

--- server/program.cs
+++ server/program.cs
@@ -1,6 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Приветствую вас.");
-Console.WriteLine("Селиванов П.А. Беляев В.А.");
-Console.WriteLine("ИСП-233");
-Console.WriteLine(DateTime.Now);
+﻿Console.WriteLine("Приветствую вас.");
 
